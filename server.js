e1280c2453ffe87bd05b045db80edbbff5e02213
Javier Rodriguez <ajr1986@gmail.com>
2018-09-06 12:01:33 -0300

    build fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/comic-app'));
+app.use(express.static(__dirname + '/dist/SPA'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/comic-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/SPA/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
